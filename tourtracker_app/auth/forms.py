f6f7c2e431684376a60f21d3016b6d43cd99238e
tw-smith <92607103+tw-smith@users.noreply.github.com>
2023-03-11 07:21:01 -0500

    adds placeholder text to forms

--- tourtracker_app/auth/forms.py
+++ tourtracker_app/auth/forms.py
@@ -4,24 +4,47 @@ from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Username',
+                        validators=[DataRequired()],
+                        render_kw={"placeholder": "Email"})
+
+    password = PasswordField('Password',
+                             validators=[DataRequired()],
+                             render_kw={"placeholder": "Password"})
+
     submit = SubmitField('Log In')
 
 
 class SignupForm(FlaskForm):
-    email = EmailField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8), EqualTo('repeat_password')])
-    repeat_password = PasswordField('Repeat Password', validators=[DataRequired(), Length(min=8)])
+    email = EmailField('Email',
+                       validators=[DataRequired(), Email()],
+                       render_kw={"placeholder": "Email address"})
+
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=8), EqualTo('repeat_pw')],
+                             render_kw={"placeholder": "Password"})
+
+    repeat_pw = PasswordField('Repeat Password',
+                              validators=[DataRequired(), Length(min=8)],
+                              render_kw={"placeholder": "Confirm Password"})
+
     submit = SubmitField('Sign Up')
 
 
 class PasswordResetRequestForm(FlaskForm):
-    email = EmailField('Email', validators=[DataRequired(), Email()])
+    email = EmailField('Email', validators=[DataRequired(), Email()],
+                       render_kw={"placeholder": "Email Address"})
+
     submit = SubmitField('Reset Password')
 
 
 class PasswordResetForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8), EqualTo('repeat_password')])
-    repeat_password = PasswordField('Repeat Password', validators=[DataRequired(), Length(min=8)])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=8), EqualTo('repeat_pw')],
+                             render_kw={"placeholder": "New Password"})
+
+    repeat_pw = PasswordField('Repeat Password',
+                              validators=[DataRequired(), Length(min=8)],
+                              render_kw={"placeholder": "Confirm Password"})
+
     submit = SubmitField('Sign Up')
