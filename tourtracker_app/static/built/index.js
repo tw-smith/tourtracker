880c1be02383f4ec48645d786522170a99e97023
tw-smith <92607103+tw-smith@users.noreply.github.com>
2023-05-08 20:11:18 -0500

    implements menu modal behaviour

--- tourtracker_app/static/built/index.js
+++ tourtracker_app/static/built/index.js
@@ -3,18 +3,18 @@ el.addEventListener('click', toggleHamburgerMenu);
 const hamburgerMenu = document.querySelector('.hamburgerMenu');
 const openMenuIcon = document.getElementById('openIcon');
 const closeMenuIcon = document.getElementById('closeIcon');
+const bodyElement = document.getElementsByTagName("body")[0];
 function toggleHamburgerMenu() {
-    console.log('hamburger toggles');
-    console.log(hamburgerMenu.classList);
     if (hamburgerMenu.classList.contains('showHamburgerMenu')) {
         hamburgerMenu.classList.toggle("showHamburgerMenu");
-        // hamburgerMenu.classList.remove("showHamburgerMenu");
         closeMenuIcon.style.display = "none";
         openMenuIcon.style.display = "block";
+        bodyElement.classList.toggle('preventScroll');
     }
     else {
         hamburgerMenu.classList.toggle("showHamburgerMenu");
         closeMenuIcon.style.display = "block";
         openMenuIcon.style.display = "none";
+        bodyElement.classList.toggle('preventScroll');
     }
 }
--- tourtracker_app/static/src/index.ts
+++ tourtracker_app/static/src/index.ts
@@ -3,19 +3,19 @@ el.addEventListener('click', toggleHamburgerMenu)
 
 const hamburgerMenu = document.querySelector('.hamburgerMenu')
 const openMenuIcon = document.getElementById('openIcon');
-const closeMenuIcon= document.getElementById('closeIcon')
+const closeMenuIcon= document.getElementById('closeIcon');
+const bodyElement = document.getElementsByTagName("body")[0]
 
 function toggleHamburgerMenu(): void {
-    console.log('hamburger toggles')
-    console.log(hamburgerMenu.classList)
     if (hamburgerMenu.classList.contains('showHamburgerMenu')) {
         hamburgerMenu.classList.toggle("showHamburgerMenu")
-        // hamburgerMenu.classList.remove("showHamburgerMenu");
         closeMenuIcon.style.display = "none";
         openMenuIcon.style.display = "block";
+        bodyElement.classList.toggle('preventScroll')
     } else {
         hamburgerMenu.classList.toggle("showHamburgerMenu")
         closeMenuIcon.style.display = "block";
         openMenuIcon.style.display = "none";
+        bodyElement.classList.toggle('preventScroll')
     }
 }
