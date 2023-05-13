083bd943b9135204463aaf4968403db295acc93d
tw-smith <92607103+tw-smith@users.noreply.github.com>
2023-05-13 07:37:19 -0500

    adds JWT issue and validation tests

--- tests/test_tourtracker.py
+++ tests/test_tourtracker.py
@@ -144,6 +144,51 @@ class TestTourTracker(unittest.TestCase):
         assert 'name="repeat_pw"' in html
         assert 'name="submit"' in html
 
+    def test_jwt_encode_decode(self):
+        jwt_user = User(
+            email='jwt@test.com',
+            password='testpassword'
+        )
+        token = jwt_user.create_token()
+        decoded_token = jwt_user.decode_token(token)
+        assert decoded_token['uuid'] == jwt_user.uuid
+        assert decoded_token['iss'] == self.app.config['JWT_ISSUER']
+
+    def test_register_email_validation(self):
+        jwt_user = User(
+            email='jwt@test.com',
+            password='testpassword'
+        )
+        db.session.add(jwt_user)
+        db.session.commit()
+        token = jwt_user.create_token()
+        response = self.client.get('/auth/verify/' + token, follow_redirects=True)
+        html = response.data.decode()
+        assert response.status_code == 200
+        assert 'Signup successful' in html
+        assert response.request.path == '/auth/login'
+        assert jwt_user.verified is True
+        db.session.delete(jwt_user)
+        db.session.commit()
+
+    def test_register_email_validation_bad_token(self):
+        jwt_user = User(
+            email='jwt@test.com',
+            password='testpassword'
+        )
+        db.session.add(jwt_user)
+        db.session.commit()
+        token = jwt_user.create_token()
+        bad_token = 'junktoken'
+        response = self.client.get('/auth/verify/' + bad_token, follow_redirects=True)
+        html = response.data.decode()
+        assert response.status_code == 200
+        assert 'Verification error' in html
+        assert response.request.path == '/auth/signup'
+        assert jwt_user.verified is False
+        db.session.delete(jwt_user)
+        db.session.commit()
+
     def test_register_user(self):
         data = {
             'email': 'testreg@test.com',
@@ -202,6 +247,32 @@ class TestTourTracker(unittest.TestCase):
         html = response.get_data(as_text=True)
         assert 'Password reset link sent' in html
         assert response.request.path == '/auth/login'
+
+    def test_reset_password(self):
+        jwt_user = User(
+            email='jwt@test.com',
+            password='testpassword'
+        )
+        db.session.add(jwt_user)
+        jwt_user.verified = True
+        db.session.commit()
+        token = jwt_user.create_token()
+        response = self.client.get('/auth/resetpassword/' + token, follow_redirects=True)
+        assert response.status_code == 200
+        html = response.data.decode()
+        assert 'name="submit"' in html
+        assert 'name="password"' in html
+        assert 'name="repeat_pw' in html
+
+        post_response = self.client.post('/auth/resetpassword/' + token, data={
+            'password': 'new_password',
+            'repeat_pw': 'new_password'
+        }, follow_redirects=True)
+        assert post_response.status_code == 200
+        assert post_response.request.path == '/auth/login'
+        assert jwt_user.check_password('new_password') is True
+        post_html = post_response.data.decode()
+        assert 'Password reset. Please log in' in post_html
 
     def test_admin_user_profile_page(self):
         self.login_admin()
